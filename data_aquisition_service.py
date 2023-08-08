2124e18c8ed6c576a96d2ee8874af90dd589ef43
Alexander Kiko <elkikos@gmail.com>
2023-08-08 23:30:31 +0200

    tidy up

--- data_aquisition_service.py
+++ data_aquisition_service.py
@@ -3,11 +3,11 @@ import pigpio
 import mh_z19
 from datetime import datetime
 
-from ..models.measurement import Measurement, MeasurementType
+from models.measurement import Measurement, MeasurementType
 
-from ..peripherals.Camera import Camera
-from ..peripherals.DHT22 import DHT22
-from ..peripherals.HC_SR04 import HC_SR04
+from peripherals.Camera import Camera
+from peripherals.DHT22 import DHT22
+from peripherals.HC_SR04 import HC_SR04
 
 class DataAquisition:
     
@@ -20,16 +20,20 @@ class DataAquisition:
         self.cam = Camera("/home/alex/pics")
         self.dht22 = DHT22(self.pigPio, 4)
         self.mhz19 = mh_z19
-        self.hc_sr04: HC_SR04 = HC_SR04(self.pigPio, 23, 24)
+        self.hc_sr04: HC_SR04 = HC_SR04(self.pigPio)
     
     def run(self):
+        # clear measurements
+        self.measurements = []
         # Every minute, take a picture
         if self.rememberedMinute != time.localtime().tm_min:
             self.imagePath = self.cam.takePicture()
             self.measurements.append(Measurement(datetime.now(), MeasurementType.Picture, self.imagePath, "path"))
+            self.rememberedMinute = time.localtime().tm_min
         
         # Temperature and Humidity 
         self.dht22.trigger()
+        time.sleep(0.1)
         temp = self.dht22.temperature()
         humidity = self.dht22.humidity()
         
@@ -37,26 +41,27 @@ class DataAquisition:
         self.measurements.append(Measurement(datetime.now(), MeasurementType.HUMIDITY, humidity, "%"))
         
         # CO2
-        try:
-            co2 = self.mhz19.read()["co2"]
-        except:
-            co2 = -1
-        self.measurements.append(Measurement(datetime.now(), MeasurementType.CO2, co2, "ppm"))
+        # try:
+        #     co2 = self.mhz19.read()["co2"]
+        # except:
+        #     co2 = -1
+        # self.measurements.append(Measurement(datetime.now(), MeasurementType.CO2, co2, "ppm"))
         
-        datetime.datetime.now()
         # Water Level
-        level = self.hc_sr04.calculatePercentFilled()
+        level = self.hc_sr04.calculatePercentFilled(40)
         self.measurements.append(Measurement(datetime.now(), MeasurementType.WaterLevel, level, "%"))
         
     def __str__(self):
         returnStr = "Measurements:\n"
         for measurement in self.measurements:
             returnStr += f"{measurement.measurement_type}({measurement.value} {measurement.unit})\n"
-            
+        return returnStr
 
 # Run a test measurement
 if __name__ == '__main__':
     aqqui = DataAquisition()
-    aqqui.run()
-    print(aqqui)
+    while True:
+        time.sleep(1)
+        aqqui.run()
+        print(aqqui)
     
