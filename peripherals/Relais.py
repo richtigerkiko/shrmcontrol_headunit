2c33465fd2cd0d53c5be5919ad811a956944e036
Alexander Kiko <elkikos@gmail.com>
2023-08-10 00:49:33 +0200

    relais implementation

--- peripherals/Relais.py
+++ peripherals/Relais.py
@@ -1,7 +1,7 @@
 import pigpio
 
 class Relais:
-    def __init__(self, pi, channel1:int = 23, channel2:int = 24):
+    def __init__(self, pi:pigpio.pi, channel1:int = 23, channel2:int = 24):
         self.channel1 = channel1
         self.channel2 = channel2
         self.pi = pi
@@ -10,4 +10,12 @@ class Relais:
         
     def on(self, channel:int):
         if channel == 1:
-            self.pi
+            self.pi.write(self.channel1, 1)
+        elif channel == 2:
+            self.pi.write(self.channel2, 1)
+
+    def off(self, channel:int):
+        if channel == 1:
+            self.pi.write(self.channel1, 0)
+        elif channel == 2:
+            self.pi.write(self.channel2, 0)
