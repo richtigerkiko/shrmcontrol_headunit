cea3b79d9444881d332a24a2663ebeec451978b5
Alexander Kiko <elkikos@gmail.com>
2023-08-09 11:14:35 +0200

    added new apicall

--- api_service.py
+++ api_service.py
@@ -1,8 +1,10 @@
 
+import datetime
 import json
 import requests
 
 from models.measurement import Measurement, MeasurementType
+from models.request.PostMeasurementObj import PostMeasurementObj
 
 
 class API_Service:
@@ -21,30 +23,25 @@ class API_Service:
     
     def sendMeasurements(self, measurements: list[Measurement]) -> None:
         pictureList = list(filter(lambda x: x.measurement_type == MeasurementType.PICTURE, measurements))
+        
         if len(pictureList) > 0:
             picture = pictureList[0]
             savedate = picture.timestamp
             measurements.remove(picture)
             picturePath = self.sendPicture(picture.value)
             measurements.append(Measurement(savedate, MeasurementType.PICTURE, picturePath, "path"))
-        jsonData = []
-        for measurement in measurements:
-            jsonData.append(measurement.__dict__)
-        self.session.post(f"{self.url}/measurements", json=jsonData)
+        
+        requestObj: PostMeasurementObj = PostMeasurementObj(measurements)
+        jsonData = json.dumps(requestObj, cls=API_Service.DateTimeEncoder)
+        
+        self.session.post(f"{self.url}/Sensor/UploadData", json=jsonData)
         pass
 
 
     def sendPicture(self, imagePath: str) -> str:
-        # self.session.headers = {'Content-Type: multipart/form-data'}
-        # self.session.headers.update({'Content-Type: multipart/form-data'})
-        # r = self.session.post(f"{self.url}/Sensor/UploadImage", files={'file': f})
-        
         try:
             files = {'file': open(imagePath, 'rb')}
             r = requests.post(f"{self.url}/Sensor/UploadImage", files=files)
             return r.text
         except:
             print("AAA")
-                
-
-            
---
+++ models/request/PostMeasurementObj.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+import socket
+
+from models.measurement import Measurement
+
+
+class PostMeasurementObj:
+
+    def __init__(self, measurements: list[Measurement]) -> None:
+        self.timestamp = datetime.now()
+        self.headunitHostname = socket.gethostname()
+        self.measurements = measurements
+        pass
+    
