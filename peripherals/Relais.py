c4fca3a34e997e54b0d985c037ad36a2ec596a26
Alexander Kiko <elkikos@gmail.com>
2023-08-10 10:18:57 +0200

    make it nice

--- peripherals/Relais.py
+++ peripherals/Relais.py
@@ -2,29 +2,22 @@ import time
 import pigpio
 
 class Relais:
-    def __init__(self, pi:pigpio.pi, channel1:int = 23, channel2:int = 24):
-        self.channel1 = channel1
-        self.channel2 = channel2
+    def __init__(self, pi:pigpio.pi, channelPins:list[int]):
         self.pi = pi
-        self.pi.set_mode(self.channel1, pigpio.OUTPUT)
-        self.pi.set_mode(self.channel2, pigpio.OUTPUT)
+        self.channelPins = channelPins
+        for pin in self.channelPins:
+            self.pi.set_mode(pin, pigpio.OUTPUT)
         
     def on(self, channel:int):
-        if channel == 1:
-            self.pi.write(self.channel1, 1)
-        elif channel == 2:
-            self.pi.write(self.channel2, 1)
+        self.pi.write(self.channelPins[channel - 1], 1)
 
     def off(self, channel:int):
-        if channel == 1:
-            self.pi.write(self.channel1, 0)
-        elif channel == 2:
-            self.pi.write(self.channel2, 0)
+        self.pi.write(self.channelPins[channel - 1], 0)
 
 
 if __name__ == "__main__":
     pi = pigpio.pi()
-    relais = Relais(pi)
+    relais = Relais(pi, [23, 24])
     relais.on(1)
     time.sleep(2)
     relais.on(2)
